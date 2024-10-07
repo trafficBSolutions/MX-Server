2e0a429a6325f9f63cf691a247dea463284ca881
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-06 22:16:25 -0400

    Update and rename windowUser.js to windowRoute.js

---
+++ routes/windowRoute.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { submitWindow } = require('../controls/windowControl');
+
+router.use(
+    cors({
+        credentials: true,
+        origin: 'https://www.materialworx.com'
+    })
+);
+
+// Use bodyParser to parse URL-encoded and JSON data
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+router.post('/window-frost-tint', submitWindow);
+
+module.exports = router;
