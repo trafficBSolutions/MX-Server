e716bfb6d0fbf93824f77fa44565e7f36924fe6b
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-09 08:25:17 -0400

    Add files via upload

---
+++ controls/shirtControl.js
@@ -0,0 +1,376 @@
+const ApparelUser = require('../users/shirtUser');
+const transporter = require('../utils/emailConfigMX'); // Use transporter2 only
+const myEmail = 'tbsolutions9@gmail.com';
+/*
+const userEmail = 'tbsolutions4@gmail.com';
+const mainEmail = 'tbsolutions3@gmail.com';
+const foreemail = 'tbsolutions55@gmail.com';
+*/
+const submitApparel = async (req, res) => {
+    try {
+        const {
+            first,
+            last,
+            company,
+            email,
+            phone,
+            address,
+            city,
+            state,
+            zip,
+            apparel,
+            message
+        } = req.body;
+
+        const apparels = JSON.parse(req.body.apparels); // Parse the apparel array from the request body
+
+        // Ensure that file upload exists
+        if (!req.files || !req.files['img'] || !req.files['img'][0]) {
+            return res.status(400).json({
+                error: "Logo is missing"
+            });
+        }
+
+        const Img = req.files['img'][0].filename;
+
+        // Validate email address
+        const isValidEmail = /\S+@\S+\.\S+/.test(email);
+        if (!isValidEmail) {
+            return res.status(400).json({
+                error: "Invalid email address"
+            });
+        }
+
+        // Create user record
+        const newUser = await ApparelUser.create({
+            first,
+            last,
+            company,
+            email,
+            phone,
+            address,
+            city,
+            state,
+            zip,
+            apparel,
+            img: Img,
+            message
+        });
+
+        // Format the submitted signs for the email body
+        let apparelsHtml = '';
+        if (apparels.length > 0) {
+            apparels.forEach((apparel, index) => {
+                apparelsHtml += `
+                    <li text-decoration: none;
+    list-style: none;>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        "> <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        "><b>Apparel Type:</b> ${apparel.type}</p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        "> <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        "><b>Apparel Size:</b> ${apparel.size}</p>
+                        
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        "> <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        "><b>Apparel Color:</b> ${apparel.color}</p>
+
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        "> <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        "><b>Apparel Quantity:</b> ${apparel.quantity}</p>
+                    </li>
+                `;
+            });
+            apparelsHtml += `</ul>`;
+        } else {
+            apparelsHtml = '<p>No apparel submitted.</p>';
+        }
+
+        // Compose email options with the apparel included
+        const mailOptions = {
+            from: 'Material WorX <tbsolutions9@gmail.com>',
+            to: email,
+            bcc: [
+                { name: 'Material WorX', address: myEmail },
+                /*
+                { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
+                
+                { name: 'Bryson Davis', address: mainEmail },
+                { name: 'Jonkell Tolbert', address: foreemail }
+                 */
+            ],
+            subject: 'CUSTOMIZABLE APPAREL REQUEST',
+            html: `
+            <!DOCTYPE html>
+            <html lang="en">
+                    <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #e7e7e7;">
+                <form style="background-color: #e7e7e7; flex-direction: column; align-items: center; justify-content: center;" action="#" method="post">
+                    <header style="background-color: #1dd2ff">
+                    <h2 style="margin-top: 20px;
+                    font-size: 50px;
+                    text-align: center;
+                    font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                    color:#000000;"
+                    >Material WorX</h2>
+                    </header>
+                   
+                    <h2 style="margin-top: 20px;
+                    font-size: 47px;
+                    text-align: center;
+                    font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                    color:#000000;"
+                    >CUSTOMIZABLE APPAREL REQUEST</h2>
+                            <div style="margin-bottom: 15px;">
+                        <h1 style="margin-top: 10px;
+                                    font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
+                            >Dear ${first},</h1>
+                        <h1 style="margin-top: 5px;
+                            font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
+                            >Your Apparel Submission has been received successfully! We will be with you within 48 hours!</h1>
+                        
+                        <h1 style="
+                        color:#000000;
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 40px;
+                        font-size: 60px;
+                        ">Contact Info:</h1>
+        
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">First Name: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${first}</p></p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">Last Name: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${last}</p></p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">Company: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${company}</p></p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">Email: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${email}</p></p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">Phone: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${phone}</p></p>
+        
+                        <h1 style="
+                        color:#000000;
+                        font-family: 'Kairos W04 Extended Bold';
+                        font-style: normal;
+                        margin-top: 40px;
+                        font-size: 60px;
+                        ">Company Address:</h1>
+        
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">Address: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${address}</p></p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">City: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${city}</p></p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">State: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${state}</p></p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        ">Zip: <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${zip}</p></p>
+
+                        <h1 style="
+                        color:#000000;
+                        font-family: 'Kairos W04 Extended Bold';
+                        font-style: normal;
+                        margin-top: 40px;
+                        font-size: 60px;
+                        ">Apparel Needed:</h1>
+
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        "> <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${apparelsHtml}</p></p>
+                        <p style="
+                        color:#000000;
+                        font-family: 'Kairos W04 Extended Bold';
+                        font-style: normal;
+                        margin-top: 40px;
+                        font-size: 60px;
+                        ">Message:</p>
+                        <p style="
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        font-style: normal;
+                        margin-top: 20px;
+                        font-size: 40px;
+                        "> <p style="
+                        margin-top: 10px;
+                        font-size: 30px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        ">${message}</p></p>
+                        <h1 style="
+                        margin-top: 80px;
+                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                        line-height: 26px;
+                        ">At MX, we greatly value your business. We wanted to inform you that your Custom Apparel request has been successfully submitted. Thank you for taking proactive steps to ensure that your business is well-represented.
+                        Our team will now review your request thoroughly to ensure how your job needs to be address. If any further information or revisions are needed, 
+                        we will promptly reach out to you.
+                        We appreciate your cooperation in this matter and look forward to working together to maintain your needs.
+                        </h1>
+                        <h1 style="margin-top: 20px;
+                                font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                                line-height: 26px;">
+                                    Best Regards,</h1>
+                        <h1 style="
+                        font-size: 30px;
+                        margin-top: 20px;
+                        font-family: 'Kairos W04 Extended Bold, Arial, Helvetica, sans-serif;
+                        line-height: 30px;
+                        ">Bryson Davis: 706-263-0175</h1>
+                        <div style="padding-top: 10px;">
+                            <h3 style="
+                            font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
+                            font-style: normal;
+                            margin-top: 20px;
+                            font-size: 40px;
+                            color:#000000;
+                            ">Contact Information:</h3>
+                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
+                            >Bryson C Davis</h1>
+                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
+                            >Traffic and Barrier Solutions, LLC</h1>
+                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
+                            >723 N Wall Street</h1>
+                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
+                            >Calhoun, GA 30701</h1>
+                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
+                            >Cell: 706-263-0175</h1>
+                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;">Website: <a href="http://www.trafficbarriersolutions.com">www.trafficbarriersolutions.com</a></h1>
+                        </div>
+                        </div>
+                        </form>
+                        </body>
+            </html>`,
+            attachments: [{
+                filename: Img,
+                path: `./files/${Img}`
+            }]
+        };
+
+        // Send email
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log('Error sending email notification:', error);
+            } else {
+                console.log('Email notification sent:', info.response);
+            }
+        });
+
+        const response = {
+            message: 'Custom Apparel submitted successfully',
+            newUser: newUser // Include the newUser object in the response
+        };
+
+        res.status(201).json(response);
+
+    } catch (error) {
+        console.error('Error submitting Custom Apparel:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+module.exports = {
+    submitApparel
+};
