3315a8a95cd3d51f79d6dd003e84e2045ae384be
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-17 17:18:23 -0400

    Update dryControl.js

--- controls/dryControl.js
+++ controls/dryControl.js
@@ -68,12 +68,9 @@ const submitDry = async (req, res) => {
             to: email,
             bcc: [
                 { name: 'Material WorX', address: myEmail },
-                /*
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
-                
                 { name: 'Bryson Davis', address: mainEmail },
                 { name: 'Jonkell Tolbert', address: foreemail }
-                 */
             ],
             subject: 'DRYWALL/CONCRETE/FLOOR GRAPHIC REQUEST',
             html: `
