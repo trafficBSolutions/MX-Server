af6fb50497d2dc43370a435836d4b454ef16f8e1
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-17 11:55:23 -0400

    Update contactRoute.js

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -7,7 +7,7 @@ const { submitContact } = require('../controls/contactControl');
 router.use(
     cors({
         credentials: true,
-        origin: 'https://www.materialworx.com'
+        origin: 'https://materialworx.netlify.app/'
     })
 );
 
