df7a46ff953f3995071416a1ef964348427220cf
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-17 17:15:28 -0400

    Update signControl.js

--- controls/signControl.js
+++ controls/signControl.js
@@ -1,11 +1,9 @@
 const SignUser = require('../users/signUser');
-const transporter = require('../utils/emailConfigMX'); // Use transporter2 only
+const transporter4 = require('../utils/emailConfigMX'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
-/*
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
 const foreemail = 'tbsolutions55@gmail.com';
-*/
 const submitSign = async (req, res) => {
     try {
         const {
@@ -387,7 +385,7 @@ const submitSign = async (req, res) => {
         };
 
         // Send email
-        transporter.sendMail(mailOptions, (error, info) => {
+        transporter4.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log('Error sending email notification:', error);
             } else {
