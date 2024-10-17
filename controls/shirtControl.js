a04d86178b6259602e5326fd94898ad265fdb545
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-17 17:15:06 -0400

    Update shirtControl.js

--- controls/shirtControl.js
+++ controls/shirtControl.js
@@ -1,11 +1,9 @@
 const ApparelUser = require('../users/shirtUser');
-const transporter = require('../utils/emailConfigMX'); // Use transporter2 only
+const transporter4 = require('../utils/emailConfigMX'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
-/*
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
 const foreemail = 'tbsolutions55@gmail.com';
-*/
 const submitApparel = async (req, res) => {
     try {
         const {
@@ -349,7 +347,7 @@ const submitApparel = async (req, res) => {
         };
 
         // Send email
-        transporter.sendMail(mailOptions, (error, info) => {
+        transporter4.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log('Error sending email notification:', error);
             } else {
