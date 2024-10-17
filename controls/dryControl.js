7f04f9a932050db78755261b3eb0e873f9d4bb15
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-10-17 17:26:33 -0400

    Update dryControl.js

--- controls/dryControl.js
+++ controls/dryControl.js
@@ -1,5 +1,5 @@
 const DryUser = require('../users/dryUser');
-const transporter4 = require('../utils/emailConfigMX'); // Use transporter2 only
+const transporter4 = require('../utils/emailConfig');
 const myEmail = 'tbsolutions9@gmail.com';
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
