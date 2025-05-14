590c689c54c7646abfcfe63257853f03572bede3
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-14 12:02:43 -0400

    Update dryControl.js

--- controls/dryControl.js
+++ controls/dryControl.js
@@ -1,6 +1,7 @@
 const DryUser = require('../users/dryUser');
-const transporter4 = require('../utils/emailConfig');
+const transporter4 = require('../utils/emailConfigMX'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
+
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
 const foreemail = 'tbsolutions55@gmail.com';
@@ -8,30 +9,32 @@ const foreemail = 'tbsolutions55@gmail.com';
 const submitDry = async (req, res) => {
     try {
         const {
-            first,
-            last,
+            name,
             company,
             email,
             phone,
-            address,
-            city,
-            state,
-            zip,
             vinylSize,
             placement,
-            finishing,
             message
         } = req.body;
-
-       
-
         // Ensure that file upload exists
         if (!req.files || !req.files['img'] || !req.files['img'][0]) {
             return res.status(400).json({
                 error: "Logo is missing"
             });
         }
+        let { finishing } = req.body;
+finishing = typeof finishing === 'string' ? finishing.trim() : '';
 
+if (!finishing || finishing.trim() === '') {
+  return res.status(400).json({ error: 'Please select a finishing option' });
+}
+        const terms = req.body.terms === 'true' || req.body.terms === true ? true : false;
+                // Ensure that file upload exists
+        const uploadedImages = req.files['img'].map(file => ({
+            filename: file.originalname,
+            path: `./files/${file.filename}`
+          }));          
         const Img = req.files['img'][0].filename;
 
         // Validate email address
@@ -44,298 +47,64 @@ const submitDry = async (req, res) => {
 
         // Create user record
         const newUser = await DryUser.create({
-            first,
-            last,
+            name,
             company,
             email,
             phone,
-            address,
-            city,
-            state,
-            zip,
             vinylSize,
             placement,
             finishing,
             img: Img,
+            terms,
             message
         });
-
-       
-
         // Compose email options
         const mailOptions = {
             from: 'Material WorX <tbsolutions9@gmail.com>',
             to: email,
             bcc: [
                 { name: 'Material WorX', address: myEmail },
+                
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
+                
                 { name: 'Bryson Davis', address: mainEmail },
                 { name: 'Jonkell Tolbert', address: foreemail }
+                 
             ],
-            subject: 'WALL MURAL/CONCRETE/FLOOR GRAPHIC REQUEST',
+            subject: 'DRYWALL/CONCRETE/FLOOR GRAPHIC REQUEST',
             html: `
-            <!DOCTYPE html>
+<!DOCTYPE html>
             <html lang="en">
                     <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #e7e7e7;">
                 <form style="background-color: #e7e7e7; flex-direction: column; align-items: center; justify-content: center;" action="#" method="post">
                     <header style="background-color: #1dd2ff">
-                    <h2 style="margin-top: 20px;
-                    font-size: 50px;
-                    text-align: center;
-                    font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                    color:#000000;"
-                    >Material WorX</h2>
+                    <h2>Material WorX</h2>
                     </header>
-                   
-                    <h2 style="margin-top: 20px;
-                    font-size: 47px;
-                    text-align: center;
-                    font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                    color:#000000;"
-                    >WALL MURAL/CONCRETE/FLOOR GRAPHIC REQUEST</h2>
+                    <h2>DRYWALL/CONCRETE/FLOOR GRAPHIC REQUEST</h2>
                             <div style="margin-bottom: 15px;">
-                        <h1 style="margin-top: 10px;
-                                    font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Dear ${first},</h1>
-                        <h1 style="margin-top: 5px;
-                            font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Your Wall Mural/Concrete/Floor Graphic submission has been received successfully! We will be with you within 48 hours!</h1>
-                        
-                        <h1 style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Contact Info:</h1>
-        
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">First Name: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${first}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Last Name: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${last}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Company: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${company}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Email: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${email}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Phone: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${phone}</p></p>
-        
-                        <h1 style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Company Address:</h1>
-        
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Address: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${address}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">City: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${city}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">State: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${state}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Zip: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${zip}</p></p>
-
-                        <h1 style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Fleet/Decal Specifications:</h1>
-                        
-                        <p style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Vinyl Size:</p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${vinylSize}</p></p>
-
-                        <p style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Placement:</p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${placement}</p></p>
-
-                        <p style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Finishing:</p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${finishing}</p></p>
-
-                        <p style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Message:</p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${message}</p></p>
-                        <h1 style="
-                        margin-top: 80px;
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        line-height: 26px;
-                        ">At MX, we greatly value your business. We wanted to inform you that your WALL MURAL/CONCRETE/FLOOR GRAPHIC request has been successfully submitted. Thank you for taking proactive steps to ensure your places are
-                        can stand out and effectively communicate your brand messages for you or for your businesses.
-                        Our team will now review your request thoroughly to ensure how you job needs to be address. If any further information or revisions are needed, 
-                        we will promptly reach out to you.
-                        We appreciate your cooperation in this matter and look forward to working together to maintain your needs.
-                        </h1>
-                        <h1 style="margin-top: 20px;
-                                font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                                line-height: 26px;">
-                                    Best Regards,</h1>
-                        <h1 style="
-                        font-size: 30px;
-                        margin-top: 20px;
-                        font-family: 'Kairos W04 Extended Bold, Arial, Helvetica, sans-serif;
-                        line-height: 30px;
-                        ">Bryson Davis: 706-263-0175</h1>
-                        <div style="padding-top: 10px;">
-                            <h3 style="
-                            font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                            font-style: normal;
-                            margin-top: 20px;
-                            font-size: 40px;
-                            color:#000000;
-                            ">Contact Information:</h3>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Bryson C Davis</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Traffic and Barrier Solutions, LLC</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >723 N Wall Street</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Calhoun, GA 30701</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Cell: 706-263-0175</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;">Website: <a href="http://www.trafficbarriersolutions.com">www.trafficbarriersolutions.com</a></h1>
-                        </div>
+                        <p>Hi <strong>${name}</strong>,</p>
+                        <p>Your Apparel Submission has been received successfully! 
+                            If you have any questions or concerns, please call (706) 263-0175.
+                            </p>
+                <h3>Contact Info:</h3> 
+      <li><p><strong>Name:</strong> ${name}</p></li>
+      <li><p><strong>Company:</strong> ${company}</p></li>
+      <li><p><strong>Email:</strong> ${email}</p></li>
+      <li><p><strong>Phone:</strong> ${phone}</p></li>
+                        <h3>Fleet/Decal Specifications:</h3>
+                        <p><strong>Vinyl Size:</strong>${vinylSize}</p>
+                        <p><strong>Placement:</strong>${placement}</p>
+                        <p><strong>Finishing:</strong>${finishing}</p>
+                        <h3>Additional Info:</h3>
+                  <li><strong>Terms & Condition:</strong> ${terms ? 'Agreed' : 'Agreed'}</li>
+                  <li><strong>${message}</strong></li>
+                        <hr style="margin: 20px 0;">
+                        <p style="font-size: 14px;">Material WorX<br>723 N Wall St, Calhoun, GA 30701<br>Phone: (706) 263-0175<br><a href="https://www.material-worx.com">www.material-worx.com</a></p>
                         </div>
                         </form>
                         </body>
             </html>`,
-            attachments: [{
-                filename: Img,
-                path: `./files/${Img}`
-            }]
+            attachments: uploadedImages
         };
 
         // Send email
@@ -348,14 +117,14 @@ const submitDry = async (req, res) => {
         });
 
         const response = {
-            message: 'Wall Mural/Concrete/Floor Graphics submitted successfully',
+            message: 'Drywall/Concrete/Floor Graphics submitted successfully',
             newUser: newUser // Include the newUser object in the response
         };
 
         res.status(201).json(response);
 
     } catch (error) {
-        console.error('Error submitting Wall Mural/Concrete/Floor Graphics:', error);
+        console.error('Error submitting Drywall/Concrete/Floor Graphics:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
