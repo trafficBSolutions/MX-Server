5aa8da64d093081d88c74586534d3a4b5e15becf
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-14 14:56:19 -0400

    Update decalControl.js

--- controls/decalControl.js
+++ controls/decalControl.js
@@ -1,5 +1,5 @@
 const DecalUser = require('../users/decalUser');
-const transporter4 = require('../utils/emailConfig');
+const transporter4 = require('../utils/emailConfigMX'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
 
 const userEmail = 'tbsolutions4@gmail.com';
@@ -9,19 +9,14 @@ const foreemail = 'tbsolutions55@gmail.com';
 const submitDecal = async (req, res) => {
     try {
         const {
-            first,
-            last,
+            name,
             company,
             email,
             phone,
-            address,
-            city,
-            state,
-            zip,
             decal,
             message
         } = req.body;
-
+const terms = req.body.terms === 'true' || req.body.terms === true ? true : false;
         const decals = JSON.parse(req.body.decals); // Parse the decals array from the request body
 
         // Ensure that file upload exists
@@ -30,7 +25,11 @@ const submitDecal = async (req, res) => {
                 error: "Logo is missing"
             });
         }
-
+        // Ensure that file upload exists
+        const uploadedImages = req.files['img'].map(file => ({
+            filename: file.originalname,
+            path: `./files/${file.filename}`
+          }));          
         const Img = req.files['img'][0].filename;
 
         // Validate email address
@@ -43,18 +42,14 @@ const submitDecal = async (req, res) => {
 
         // Create user record
         const newUser = await DecalUser.create({
-            first,
-            last,
+            name,
             company,
             email,
             phone,
-            address,
-            city,
-            state,
-            zip,
             decal,
             img: Img,
-            message
+            message,
+            terms
         });
 
         // Format the submitted signs for the email body
@@ -62,50 +57,11 @@ const submitDecal = async (req, res) => {
         if (decals.length > 0) {
             decals.forEach((decal, index) => {
                 decalsHtml += `
-                    <li text-decoration: none;
-    list-style: none;>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        "><b>Decal & Sticker Type:</b> ${decal.type}</p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        "><b>Decal & Sticker Size:</b> ${decal.size}</p>
-                        
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        "><b>Decal & Sticker Cut Type:</b> ${decal.cut}</p>
-
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        "><b>Decal & Sticker Quantity:</b> ${decal.quantity}</p>
+                    <li style="margin-bottom: 20px; padding: 10px; border: 1px solid #ccc; background-color: #f9f9f9;">
+                        <p><strong>Decal & Sticker Type:</strong> ${decal.type}</p>
+                        <p><strong>Decal & Sticker Size:</strong> ${decal.size}</p>
+                        <p><strong>Decal & Sticker Cut Type:</strong> ${decal.cut}</p>
+                        <p><strong>Decal & Sticker Quantity:</strong> ${decal.quantity}</p>
                     </li>
                 `;
             });
@@ -120,229 +76,45 @@ const submitDecal = async (req, res) => {
             to: email,
             bcc: [
                 { name: 'Material WorX', address: myEmail },
+                
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
+                
                 { name: 'Bryson Davis', address: mainEmail },
                 { name: 'Jonkell Tolbert', address: foreemail }
+                 
             ],
             subject: 'DECAL(S) & STICKER(S) REQUEST',
             html: `
-            <!DOCTYPE html>
+           <!DOCTYPE html>
             <html lang="en">
                     <body style="margin: 0; padding: 0; font-family: Arial, sans-serif; background-color: #e7e7e7;">
                 <form style="background-color: #e7e7e7; flex-direction: column; align-items: center; justify-content: center;" action="#" method="post">
                     <header style="background-color: #1dd2ff">
-                    <h2 style="margin-top: 20px;
-                    font-size: 50px;
-                    text-align: center;
-                    font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                    color:#000000;"
-                    >Material WorX</h2>
+                    <h2>Material WorX</h2>
                     </header>
-                   
-                    <h2 style="margin-top: 20px;
-                    font-size: 47px;
-                    text-align: center;
-                    font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                    color:#000000;"
-                    >DECAL(S) & STICKER(S) REQUEST</h2>
+                    <h2>DECAL(S) & STICKER(S) REQUEST</h2>
                             <div style="margin-bottom: 15px;">
-                        <h1 style="margin-top: 10px;
-                                    font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Dear ${first},</h1>
-                        <h1 style="margin-top: 5px;
-                            font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Your Decal & Sticker Submission has been received successfully! We will be with you within 48 hours!</h1>
-                        
-                        <h1 style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Contact Info:</h1>
-        
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">First Name: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${first}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Last Name: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${last}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Company: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${company}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Email: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${email}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Phone: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${phone}</p></p>
-        
-                        <h1 style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Company Address:</h1>
-        
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Address: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${address}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">City: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${city}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">State: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${state}</p></p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        ">Zip: <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${zip}</p></p>
-
-                        <h1 style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Decal(s) Needed:</h1>
-
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${decalsHtml}</p></p>
-                        <p style="
-                        color:#000000;
-                        font-family: 'Kairos W04 Extended Bold';
-                        font-style: normal;
-                        margin-top: 40px;
-                        font-size: 60px;
-                        ">Message:</p>
-                        <p style="
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        font-style: normal;
-                        margin-top: 20px;
-                        font-size: 40px;
-                        "> <p style="
-                        margin-top: 10px;
-                        font-size: 30px;
-                        font-family: Arial, Helvetica, sans-serif;
-                        ">${message}</p></p>
-                        <h1 style="
-                        margin-top: 80px;
-                        font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                        line-height: 26px;
-                        ">At MX, we greatly value your business. We wanted to inform you that your Decals & Stickers request has been successfully submitted. Thank you for taking proactive steps to ensure that your business is well-represented.
-                        Our team will now review your request thoroughly to ensure how your job needs to be address. If any further information or revisions are needed, 
-                        we will promptly reach out to you.
-                        We appreciate your cooperation in this matter and look forward to working together to maintain your needs.
-                        </h1>
-                        <h1 style="margin-top: 20px;
-                                font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                                line-height: 26px;">
-                                    Best Regards,</h1>
-                        <h1 style="
-                        font-size: 30px;
-                        margin-top: 20px;
-                        font-family: 'Kairos W04 Extended Bold, Arial, Helvetica, sans-serif;
-                        line-height: 30px;
-                        ">Bryson Davis: 706-263-0175</h1>
-                        <div style="padding-top: 10px;">
-                            <h3 style="
-                            font-family: 'Kairos W04 Extended Bold', Arial, Helvetica, sans-serif;
-                            font-style: normal;
-                            margin-top: 20px;
-                            font-size: 40px;
-                            color:#000000;
-                            ">Contact Information:</h3>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Bryson C Davis</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Traffic and Barrier Solutions, LLC</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >723 N Wall Street</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Calhoun, GA 30701</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;"
-                            >Cell: 706-263-0175</h1>
-                            <h1 style= "font-family: 'Moveo Sans w00 Regular', Arial, Helvetica, sans-serif;">Website: <a href="http://www.trafficbarriersolutions.com">www.trafficbarriersolutions.com</a></h1>
-                        </div>
+                        <p>Hi <strong>${name}</strong>,</p>
+                        <p>Your Decal and Sticker Submission has been received successfully! 
+                            If you have any questions or concerns, please call (706) 263-0175.
+                            </p>
+                        <h3>Contact Info:</h3>
+                        <li><strong>Name:</strong> ${name}</li>
+                        <li><strong>Company:</strong> ${company}</li>
+                        <li><strong>Email:</strong> ${email}</li>
+                        <li><strong>Phone:</strong> ${phone}</li>
+                       <h3>Decal Summary:</h3>
+                        <p>${decalsHtml}</p></p>
+                        <h3>Additional Info:</h3>
+                  <li><strong>Terms & Condition:</strong> ${terms ? 'Agreed' : 'Agreed'}</li>
+                  <li><strong>${message}</strong></li>
+                        <hr style="margin: 20px 0;">
+                        <p style="font-size: 14px;">Material WorX<br>723 N Wall St, Calhoun, GA 30701<br>Phone: (706) 263-0175<br><a href="https://www.material-worx.com">www.material-worx.com</a></p>
                         </div>
                         </form>
                         </body>
             </html>`,
-            attachments: [{
-                filename: Img,
-                path: `./files/${Img}`
-            }]
+attachments: uploadedImages
         };
 
         // Send email
