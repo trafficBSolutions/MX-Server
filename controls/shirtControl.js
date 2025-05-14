44dfa86e7ed31ee7a69fc28430437b77d18f2c16
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-14 12:05:02 -0400

    Update shirtControl.js

--- controls/shirtControl.js
+++ controls/shirtControl.js
@@ -72,9 +72,12 @@ const submitApparel = async (req, res) => {
             to: email,
             bcc: [
                 { name: 'Material WorX', address: myEmail },
+                
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
+                
                 { name: 'Bryson Davis', address: mainEmail },
                 { name: 'Jonkell Tolbert', address: foreemail }
+                 
             ],
             subject: 'CUSTOMIZABLE APPAREL REQUEST',
             html: `
