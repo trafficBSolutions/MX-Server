9ad6cdb0c34c34bc497ed5ed8219a33e3d371370
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-23 13:45:10 -0400

    Update windowControl.js

--- controls/windowControl.js
+++ controls/windowControl.js
@@ -1,5 +1,5 @@
 const WindowUser = require('../users/windowUser');
-const transporter4 = require('../utils/emailConfigMX'); // Use transporter2 only
+const transporter4 = require('../utils/emailConfig'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
 
 const userEmail = 'tbsolutions4@gmail.com';
