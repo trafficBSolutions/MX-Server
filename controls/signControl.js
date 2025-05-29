3f25861c4fe0cb7eb4a1d901baf6850561b4883c
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-29 15:17:01 -0400

    Update signControl.js

--- controls/signControl.js
+++ controls/signControl.js
@@ -1,5 +1,5 @@
 const SignUser = require('../users/signUser');
-const transporter4 = require('../utils/emailConfig'); // Use transporter2 only
+const transporter5 = require('../utils/emailConfig'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
 
 const userEmail = 'tbsolutions4@gmail.com';
@@ -121,7 +121,7 @@ if (signs.length > 0) {
         };
 
         // Send email
-        transporter4.sendMail(mailOptions, (error, info) => {
+        transporter5.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log('Error sending email notification:', error);
             } else {
