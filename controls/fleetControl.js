2da0e369d96350b8bdcabe6fdd3e73a4451a63b9
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-29 15:16:18 -0400

    Update fleetControl.js

--- controls/fleetControl.js
+++ controls/fleetControl.js
@@ -1,5 +1,5 @@
 const FleetUser = require('../users/fleetUser');
-const transporter4 = require('../utils/emailConfig'); // Use transporter2 only
+const transporter5 = require('../utils/emailConfig'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
 
 const userEmail = 'tbsolutions4@gmail.com';
@@ -113,7 +113,7 @@ const submitFleet = async (req, res) => {
         };
 
         // Send email
-        transporter4.sendMail(mailOptions, (error, info) => {
+        transporter5.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log('Error sending email notification:', error);
             } else {
