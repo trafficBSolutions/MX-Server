5c65d7a62bbe8c9697162979e57a72b6be62ae64
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-29 15:10:56 -0400

    Update dryControl.js

--- controls/dryControl.js
+++ controls/dryControl.js
@@ -5,7 +5,7 @@ const myEmail = 'tbsolutions9@gmail.com';
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
 const foreemail = 'tbsolutions55@gmail.com';
-
+const damien = 'tbsolutions14@gmail.com';
 const submitDry = async (req, res) => {
     try {
         const {
@@ -68,7 +68,8 @@ if (!finishing || finishing.trim() === '') {
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
                 
                 { name: 'Bryson Davis', address: mainEmail },
-                { name: 'Jonkell Tolbert', address: foreemail }
+                { name: 'Jonkell Tolbert', address: foreemail },
+                { name: 'Damien Diskey', address: damien}
                  
             ],
             subject: 'DRYWALL/CONCRETE/FLOOR GRAPHIC REQUEST',
