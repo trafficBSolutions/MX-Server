779c3924b679a23ba25922a43a872eac8ada37e7
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-05-29 15:11:23 -0400

    Update fleetControl.js

--- controls/fleetControl.js
+++ controls/fleetControl.js
@@ -5,7 +5,7 @@ const myEmail = 'tbsolutions9@gmail.com';
 const userEmail = 'tbsolutions4@gmail.com';
 const mainEmail = 'tbsolutions3@gmail.com';
 const foreemail = 'tbsolutions55@gmail.com';
-
+const damien = 'tbsolutions14@gmail.com';
 const submitFleet = async (req, res) => {
     try {
         const {
@@ -67,7 +67,8 @@ const submitFleet = async (req, res) => {
                 { name: 'Carson Speer', address: userEmail }, // Add the second Gmail address to BCC
                 
                 { name: 'Bryson Davis', address: mainEmail },
-                { name: 'Jonkell Tolbert', address: foreemail }
+                { name: 'Jonkell Tolbert', address: foreemail },
+                { name: 'Damien Diskey', address: damien}
                  
             ],
             subject: 'FLEET/DECAL VEHICLE GRAPHIC REQUEST',
