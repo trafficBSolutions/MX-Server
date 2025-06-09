4ef319672035a019e8620b8bb0b0660b38f3f270
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2025-06-09 09:08:23 -0400

    Update logoControl.js

--- controls/logoControl.js
+++ controls/logoControl.js
@@ -1,5 +1,5 @@
 const LogoUser = require('../users/logoUser');
-const transporter5 = require('../utils/emailConfig'); // Use transporter2 only
+const transporter6 = require('../utils/emailConfig'); // Use transporter2 only
 const myEmail = 'tbsolutions9@gmail.com';
 
 const userEmail = 'tbsolutions4@gmail.com';
@@ -89,7 +89,7 @@ const terms = req.body.terms === 'true' || req.body.terms === true ? true : fals
             attachments: uploadedImages
         };
         // Send email
-        transporter5.sendMail(mailOptions, (error, info) => {
+        transporter6.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log('Error sending email notification:', error);
             } else {
